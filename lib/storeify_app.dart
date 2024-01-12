450a6a84a7e02ef592ee24c5dc684529e1fff2d6
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-01-13 01:55:33 +0200

    Update main.dart & storeify_app.dart files

--- lib/storeify_app.dart
+++ lib/storeify_app.dart
@@ -4,12 +4,8 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:store_ify/config/router/app_router.dart';
 import 'package:store_ify/config/themes/app_themes.dart';
 import 'package:store_ify/core/utils/app_strings.dart';
-import 'package:store_ify/features/home/data/cubit/stores_cubit.dart';
-import 'package:store_ify/features/stores/presentation/cubit/clothes/clothes_cubit.dart';
-import 'package:store_ify/features/stores/presentation/cubit/food/food_cubit.dart';
 import 'package:store_ify/service_locator.dart';
 import 'package:store_ify/features/layout/presentation/cubit/layout_cubit.dart';
-import 'package:store_ify/features/categories/presentation/cubit/category_cubit.dart';
 
 class StoreifyApp extends StatelessWidget {
   const StoreifyApp({super.key});
@@ -20,14 +16,33 @@ class StoreifyApp extends StatelessWidget {
       designSize: const Size(375, 812),
       minTextAdapt: true,
       splitScreenMode: true,
-      builder: (context, child) {
-        return MaterialApp(
+      builder: (context, child) => MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => serviceLocator.get<LayoutCubit>(),
+          ),
+          // BlocProvider(
+          //   create: (context) =>
+          //       serviceLocator.get<CategoryCubit>()..getCategories(),
+          // ),
+          // BlocProvider(
+          //   create: (context) => serviceLocator.get<StoresCubit>()..getStores(),
+          // ),
+          // BlocProvider(
+          //   create: (context) =>
+          //       serviceLocator.get<ClothesCubit>()..getClothes(),
+          // ),
+          // BlocProvider(
+          //   create: (context) => serviceLocator.get<FoodCubit>()..getFood(),
+          // ),
+        ],
+        child: MaterialApp(
           debugShowCheckedModeBanner: false,
           theme: AppThemes.lightTheme,
           title: AppStrings.appTitle,
           onGenerateRoute: AppRouter.onGenerateRoute,
-        );
-      },
+        ),
+      ),
     );
   }
 }
