0546d2f7954bc649279b1700ae1bf25d8b50ba5d
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-01-13 00:25:41 +0200

    Add utils & NetworkInfo

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,75 @@
+abstract class AppAssets {
+  /// Assets for AppIcon
+  /// assets/app_icon.png
+  static const String appIcon = "assets/app_icon.png";
+
+  /// Assets for iconsIcOutlineNotifications
+  /// assets/icons/ic_outline-notifications.jpg
+  static const String iconsIcOutlineNotifications =
+      "assets/icons/ic_outline-notifications.jpg";
+
+  /// Assets for imagesEllipse 4
+  /// assets/images/Ellipse 4.png
+  static const String imagesPerson = "assets/images/person.png";
+
+  /// Assets for imagesFacebook
+  /// assets/images/facebook.png
+  static const String imagesFacebook = "assets/images/facebook.png";
+
+  /// Assets for imagesGoogle
+  /// assets/images/Google.png
+  static const String imagesGoogle = "assets/images/Google.png";
+
+  /// Assets for imagesImage 1
+  /// assets/images/image 1.png
+  static const String imagesOtpVerification =
+      "assets/images/otp_verification.png";
+
+  /// Assets for imagesOnBoarding1
+  /// assets/images/on_boarding1.png
+  static const String imagesOnBoarding1 = "assets/images/on_boarding1.png";
+
+  /// Assets for imagesOnBoarding2
+  /// assets/images/on_boarding2.png
+  static const String imagesOnBoarding2 = "assets/images/on_boarding2.png";
+
+  /// Assets for imagesOnBoarding3
+  /// assets/images/on_boarding3.png
+  static const String imagesOnBoarding3 = "assets/images/on_boarding3.png";
+
+  /// Assets for imagesTwitter
+  /// assets/images/twitter.png
+  static const String imagesTwitter = "assets/images/twitter.png";
+
+  static const String locationDialog = "assets/icons/carbon_map.png";
+
+  static const String search = "assets/icons/Search.png";
+
+  static const String edit = "assets/icons/Filter.png";
+
+  static const String testBestSelling = "assets/images/Best_Selling.png";
+
+  static const String profile = "assets/images/profile.png";
+
+  static const String testClothesMan = "assets/images/test_clothes.png";
+
+  static const String testClothesWoman = "assets/images/test_clothes_woman.png";
+
+  static const String categories = "assets/images/Categories.png";
+
+  static const String stores = "assets/images/Stores.png";
+
+  static const String details = "assets/images/details.png";
+
+  static const String details1 = "assets/images/details1.png";
+
+  static const String details2 = "assets/images/details2.png";
+
+  static const String circleColor = "assets/images/circle_color.png";
+
+  static const String stores1 = "assets/images/stores1.png";
+
+  static const String stores2 = "assets/images/stores2.png";
+
+  static const String test = 'assets/images/test.jpeg';
+}
