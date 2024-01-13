dfdd76cd79723caa13e5d24e5a2c33d6c333a41a
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-01-13 13:21:53 +0200

    Build CustomLoadingIndicator & Update MainButton's height

--- lib/config/themes/app_colors.dart
+++ lib/config/themes/app_colors.dart
@@ -5,7 +5,7 @@ class AppColors {
   static const Color scaffoldBackgroundColor = Colors.white;
   static const Color subTitleColor = Color(0xFF575757);
   static const Color blueColor = Color(0xFF0F3557);
-  static const Color iconsColor = Color(0xffD0CFCE);
+  static const Color greyColor = Color(0xffD0CFCE);
   static const Color lightBlueColor = Color(0xff5AABF7);
   static const Color discountColor = Color(0xff3E3D3B);
   static const Color fontPrimaryColor = Color(0xff0B2841);
