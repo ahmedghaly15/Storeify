ba95e1d84a47bc53b99076f38c712fb8ed298637
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-01-13 23:43:02 +0200

    Refactor CategoryItem & Update CategoriesView

--- lib/features/categories/presentation/view/categories_view.dart
+++ lib/features/categories/presentation/view/categories_view.dart
@@ -6,7 +6,7 @@ import 'package:store_ify/core/utils/app_constants.dart';
 import 'package:store_ify/core/widgets/custom_sliver_app_bar.dart';
 import 'package:store_ify/features/categories/data/models/categories_model.dart';
 import 'package:store_ify/features/categories/presentation/cubit/category_cubit.dart';
-import 'package:store_ify/features/categories/presentation/widgets/custom_category_item.dart';
+import 'package:store_ify/features/categories/presentation/widgets/category_item.dart';
 
 class CategoriesView extends StatelessWidget {
   const CategoriesView({super.key});
@@ -24,26 +24,34 @@ class CategoriesView extends StatelessWidget {
                 final List<CategoryModel> categories =
                     BlocProvider.of<CategoryCubit>(context).categories;
 
-                return SliverGrid(
-                  delegate: SliverChildBuilderDelegate(
-                    childCount: categories.length,
-                    addAutomaticKeepAlives: true,
-                    (context, index) => AnimationConfiguration.staggeredGrid(
-                      position: index,
-                      columnCount: categories.length,
-                      duration: const Duration(milliseconds: 650),
-                      child: ScaleAnimation(
-                        child: FadeInAnimation(
-                          child: CustomCategoryItem(
-                            category: categories[index],
+                return SliverPadding(
+                  padding: EdgeInsets.only(
+                    left: 16.w,
+                    right: 16.w,
+                    bottom: 16.h,
+                  ),
+                  sliver: SliverGrid(
+                    delegate: SliverChildBuilderDelegate(
+                      childCount: categories.length,
+                      addAutomaticKeepAlives: true,
+                      (context, index) => AnimationConfiguration.staggeredGrid(
+                        position: index,
+                        columnCount: categories.length,
+                        duration: const Duration(milliseconds: 650),
+                        child: ScaleAnimation(
+                          child: FadeInAnimation(
+                            child: CategoryItem(
+                              category: categories[index],
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 10.w,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 10.w,
+                      mainAxisSpacing: 16.h,
+                    ),
                   ),
                 );
               },
