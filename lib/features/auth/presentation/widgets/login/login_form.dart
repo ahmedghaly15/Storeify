7d9b19dec5f325bb694effb4cb1d7a027a64680b
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-07-07 08:37:27 +0300

    Built LoginButtonBlocConsumer widget & Updated LoginForm

--- lib/features/auth/presentation/widgets/login/login_form.dart
+++ lib/features/auth/presentation/widgets/login/login_form.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:store_ify/core/helpers/extensions.dart';
 import 'package:store_ify/core/helpers/auth_helper.dart';
+import 'package:store_ify/core/helpers/extensions.dart';
 import 'package:store_ify/core/locale/lang_keys.dart';
-import 'package:store_ify/core/locale/logic/cubit/locale_cubit.dart';
 import 'package:store_ify/core/themes/app_colors.dart';
-import 'package:store_ify/core/widgets/main_button.dart';
-import 'package:store_ify/core/widgets/my_sized_box.dart';
 import 'package:store_ify/core/widgets/custom_text_field.dart';
+import 'package:store_ify/core/widgets/my_sized_box.dart';
 import 'package:store_ify/features/auth/presentation/cubits/login/login_cubit.dart';
+import 'package:store_ify/features/auth/presentation/cubits/login/login_state.dart';
+import 'package:store_ify/features/auth/presentation/widgets/login/login_button_bloc_consumer.dart';
 import 'package:store_ify/features/auth/presentation/widgets/text_field_label.dart';
 
 class LoginForm extends StatelessWidget {
@@ -34,38 +34,33 @@ class LoginForm extends StatelessWidget {
           ),
           MySizedBox.height24,
           const TextFieldLabel(labelKey: LangKeys.password),
-          CustomTextField(
-            autofillHints: const <String>[AutofillHints.password],
-            validate: (String? value) =>
-                AuthHelper.validatePasswordField(value),
-            focusNode: context.read<LoginCubit>().passwordFocusNode,
-            // onSubmit: (_) {},
-            controller: context.read<LoginCubit>().passwordController,
-            keyboardType: TextInputType.visiblePassword,
-            hintText: '*********',
-            // obscureText: BlocProvider.of<LoginCubit>(context).isPassword,
-            suffixIcon: IconButton(
-              onPressed: () {
-                // BlocProvider.of<LoginCubit>(context).changePasswordVisibility();
-              },
-              icon: const Icon(
-                // BlocProvider.of<LoginCubit>(context).isPassword
-                // ? Icons.visibility_outlined
-                // : Icons.visibility_off_outlined,
-                Icons.visibility_off_outlined,
-                color: AppColors.primaryColor,
+          BlocBuilder<LoginCubit, LoginState>(
+            buildWhen: (_, current) => current is InvertPasswordVisibility,
+            builder: (context, state) => CustomTextField(
+              autofillHints: const <String>[AutofillHints.password],
+              validate: (String? value) =>
+                  AuthHelper.validatePasswordField(value),
+              focusNode: context.read<LoginCubit>().passwordFocusNode,
+              onSubmit: (_) => context.read<LoginCubit>().login(context),
+              controller: context.read<LoginCubit>().passwordController,
+              keyboardType: TextInputType.visiblePassword,
+              hintText: '*********',
+              obscureText: context.read<LoginCubit>().isPasswordVisible,
+              suffixIcon: IconButton(
+                onPressed: () {
+                  context.read<LoginCubit>().invertPasswordVisibility();
+                },
+                icon: Icon(
+                  context.read<LoginCubit>().isPasswordVisible
+                      ? Icons.visibility_outlined
+                      : Icons.visibility_off_outlined,
+                  color: AppColors.primaryColor,
+                ),
               ),
             ),
           ),
           MySizedBox.height32,
-          MainButton(
-            textKey: LangKeys.login,
-            // onPressed: () => _login(context),
-            onPressed: () {
-              // context.read<LoginCubit>().login();
-              context.read<LocaleCubit>().toggleLocale();
-            },
-          ),
+          const LoginButtonBlocConsumer(),
         ],
       ),
     );
