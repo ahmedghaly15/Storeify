7a1d17593fd5ccd10da290334888906514dad2e2
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-07-07 05:19:58 +0300

    Built IsCurrentLocaleEnglish, Translate extensions

--- lib/core/helpers/extensions.dart
+++ lib/core/helpers/extensions.dart
@@ -1,4 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:store_ify/core/locale/app_localizations.dart';
+import 'package:store_ify/core/locale/logic/cubit/locale_cubit.dart';
+import 'package:store_ify/core/utils/app_strings.dart';
+
 extension NullOrEmptyString on String? {
   /// Check if the string is null or empty
   bool get nullOrEmpty => this == null || this == '';
+}
+
+extension IsCurrentLocaleEnglish on BuildContext {
+  bool get isEnglish =>
+      read<LocaleCubit>().currentLang == AppStrings.englishLangCode;
+}
+
+extension Translate on BuildContext {
+  String translate(String key) => AppLocalizations.of(this)!.translate(key)!;
 }
