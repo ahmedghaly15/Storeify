af15b7e53b1cb882b332af753b413b8f9b968f23
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-07-07 06:28:20 +0300

    Updated build.gradle & more

--- lib/dependency_injection.dart
+++ lib/dependency_injection.dart
@@ -7,6 +7,9 @@ import 'package:store_ify/core/locale/logic/cubit/locale_cubit.dart';
 import 'package:store_ify/core/locale/logic/locale_repo.dart';
 import 'package:store_ify/core/network/network_info.dart';
 import 'package:store_ify/core/network/network_info_impl.dart';
+import 'package:store_ify/core/router/app_router.dart';
+import 'package:store_ify/features/auth/data/repos/login_repo.dart';
+import 'package:store_ify/features/auth/presentation/cubits/login/login_cubit.dart';
 
 final GetIt getIt = GetIt.instance;
 
@@ -25,16 +28,26 @@ void _setupDIForCore() {
 
   final Dio dio = DioFactory.getDio();
   getIt.registerLazySingleton<ApiService>(() => ApiService(dio));
+
+  getIt.registerSingleton<AppRouter>(AppRouter());
 }
 
 void _setupDIForRepos() {
   getIt.registerLazySingleton<LocaleRepo>(() => const LocaleRepo());
+  getIt.registerLazySingleton<LoginRepo>(
+    () => LoginRepo(getIt.get<ApiService>()),
+  );
 }
 
 void _setupDIForCubits() {
   getIt.registerFactory<LocaleCubit>(
     () => LocaleCubit(getIt.get<LocaleRepo>()),
   );
+
+  getIt.registerFactory<LoginCubit>(
+    () => LoginCubit(getIt.get<LoginRepo>()),
+  );
+
   // getIt.registerFactory<OnBoardingCubit>(
   //   () => OnBoardingCubit(onBoardingRepo: getIt.get<OnBoardingRepo>()),
   // );
