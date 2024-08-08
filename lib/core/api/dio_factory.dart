3cd32585457c55c0eb10dc53313672c47a24fc63
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-08-08 17:39:39 +0300

    Added new methods to DioFactory  - Added _addDioHeaders() method  - Added setTokenIntoHeadersAfterLogin() method

--- lib/core/api/dio_factory.dart
+++ lib/core/api/dio_factory.dart
@@ -1,5 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+import 'package:store_ify/core/helpers/shared_pref_helper.dart';
+import 'package:store_ify/core/helpers/shared_pref_keys.dart';
 
 class DioFactory {
   /// private constructor as I don't want to allow creating an instance of this class
@@ -15,6 +17,7 @@ class DioFactory {
       _dio!
         ..options.connectTimeout = timeOut
         ..options.receiveTimeout = timeOut;
+      _addDioHeaders();
       _addDioLoggerInterceptor();
       return _dio!;
     } else {
@@ -30,5 +33,19 @@ class DioFactory {
         responseHeader: true,
       ),
     );
+  }
+
+  static void _addDioHeaders() async {
+    _dio?.options.headers = {
+      'Accept': 'application/json',
+      'Authorization':
+          'Bearer ${await SharedPrefHelper.getSecuredString(SharedPrefKeys.userToken)}',
+    };
+  }
+
+  static void setTokenIntoHeadersAfterLogin(String token) {
+    _dio?.options.headers = {
+      'Authorization': 'Bearer $token',
+    };
   }
 }
