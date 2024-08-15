9cee5b3e84c594c64c0348af65bc654c7894a562
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-08-15 12:43:29 +0300

    Finished SubCategory Cubit/State

---
+++ lib/features/categories/presentation/cubit/sub_category/sub_category_cubit.dart
@@ -0,0 +1,32 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:store_ify/features/categories/data/models/fetch_sub_category_params.dart';
+import 'package:store_ify/features/categories/data/repositories/categories_repo.dart';
+import 'package:store_ify/features/categories/presentation/cubit/sub_category/sub_category_state.dart';
+
+class SubCategoryCubit extends Cubit<SubCategoryState> {
+  SubCategoryCubit(
+    this._categoriesRepo,
+  ) : super(const SubCategoryState.initial());
+
+  final CategoriesRepo _categoriesRepo;
+  final CancelToken _cancelToken = CancelToken();
+
+  void fetchSubCategory(FetchSubCategoryParams params) async {
+    emit(const SubCategoryState.fetchSubCategoryLoading());
+    final result = await _categoriesRepo.fetchSubCategory(params, _cancelToken);
+    result.when(
+      success: (subCategory) =>
+          emit(SubCategoryState.fetchSubCategorySuccess(subCategory)),
+      error: (error) => emit(
+        SubCategoryState.fetchSubCategoryError(error.apiErrorModel.error ?? ''),
+      ),
+    );
+  }
+
+  @override
+  Future<void> close() {
+    _cancelToken.cancel();
+    return super.close();
+  }
+}
