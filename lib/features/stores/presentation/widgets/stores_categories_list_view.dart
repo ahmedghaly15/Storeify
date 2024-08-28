360d07e0710f80406da57a3f1c86c14e764378e9
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-08-28 13:50:53 +0300

    Updated StoreDetailsData widgets  - Updated StoreOffersGridViewBlocBuilder, StoreBranchesListViewBlocBuilder, StoreCategoriesGridViewBlocBuilder

---
+++ lib/features/stores/presentation/widgets/stores_categories_list_view.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:store_ify/core/themes/app_colors.dart';
+import 'package:store_ify/core/themes/app_text_styles.dart';
+import 'package:store_ify/core/utils/app_constants.dart';
+import 'package:store_ify/core/widgets/custom_outlined_button.dart';
+import 'package:store_ify/core/widgets/my_sized_box.dart';
+import 'package:store_ify/features/stores/data/models/fetch_stores_response.dart';
+import 'package:store_ify/features/stores/presentation/cubits/stores/stores_cubit.dart';
+import 'package:store_ify/features/stores/presentation/cubits/stores/stores_state.dart';
+
+class StoreCategoriesListView extends StatelessWidget {
+  const StoreCategoriesListView({super.key, required this.fetchStoresResponse});
+
+  final FetchStoresResponse fetchStoresResponse;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      padding: AppConstants.categoryPadding,
+      itemBuilder: (_, index) => BlocBuilder<StoresCubit, StoresState>(
+        buildWhen: (_, current) => current is UpdateCurrentSelectedStore,
+        builder: (context, state) {
+          int currentStoreIndex =
+              context.read<StoresCubit>().currentSelectedStore;
+          return CustomOutlinedButton(
+            foregroundColor: _activeColor(
+                currentStoreIndex, fetchStoresResponse.stores[index].id),
+            borderColor: _activeColor(
+                currentStoreIndex, fetchStoresResponse.stores[index].id),
+            onPressed: () => _updateSelectedStoreAndItsIndex(context, index),
+            child: Text(
+              fetchStoresResponse.stores[index].name,
+              style: AppTextStyles.textStyle10Medium.copyWith(
+                color: _activeColor(
+                    currentStoreIndex, fetchStoresResponse.stores[index].id),
+              ),
+            ),
+          );
+        },
+      ),
+      separatorBuilder: (_, __) => MySizedBox.width8,
+      itemCount: fetchStoresResponse.stores.length,
+    );
+  }
+
+  void _updateSelectedStoreAndItsIndex(BuildContext context, int index) {
+    context
+        .read<StoresCubit>()
+        .updateCurrentSelectedStore(fetchStoresResponse.stores[index].id);
+    context.read<StoresCubit>().updateCurrentStoreIndex(index);
+  }
+
+  bool _isActive(int currentStoreIndex, int storeId) =>
+      currentStoreIndex == storeId;
+  Color _activeColor(int currentStoreIndex, int storeId) =>
+      _isActive(currentStoreIndex, storeId)
+          ? AppColors.primaryColor
+          : AppColors.blueColor;
+}
