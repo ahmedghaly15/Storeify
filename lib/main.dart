77b1b6c91f91d795f560faaa969270d483811ab0
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-09-11 00:19:20 +0300

    Updated main method

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:store_ify/core/helpers/hive_helper.dart';
 import 'package:store_ify/core/utils/functions/check_if_onboarding_visited.dart';
 import 'package:store_ify/core/utils/functions/check_if_user_is_logged_in.dart';
 import 'package:store_ify/storeify_app.dart';
@@ -8,6 +11,9 @@ import 'package:store_ify/dependency_injection.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  final appDocumentDir = await getApplicationDocumentsDirectory();
+  await Hive.initFlutter(appDocumentDir.path);
+  await HiveHelper.registerAdapters();
   setupDI();
   Bloc.observer = MyBlocObserver();
   await checkIfOnboardingIsVisited();
