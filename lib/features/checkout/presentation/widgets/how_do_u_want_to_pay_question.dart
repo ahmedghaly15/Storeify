7131aae584b5db6fd3765dbe85f00ac5dd8acf15
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-09-11 20:52:11 +0300

    Added animation to PaymentMethod feature's widgets

--- lib/features/checkout/presentation/widgets/how_do_u_want_to_pay_question.dart
+++ lib/features/checkout/presentation/widgets/how_do_u_want_to_pay_question.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:store_ify/core/helpers/extensions.dart';
@@ -11,17 +12,20 @@ class HowDoUWantToPayQuestion extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsetsDirectional.only(
-        start: AppConstants.mainButtonHorizontalMarginVal.w,
-        end: AppConstants.mainButtonHorizontalMarginVal.w,
-        top: 32.h,
-        bottom: 40.h,
-      ),
-      child: Text(
-        context.translate(LangKeys.howDoUWantToPay),
-        style: AppTextStyles.textStyle16Medium.copyWith(
-          color: AppColors.primaryColor,
+    return FadeInDown(
+      from: 30.h,
+      child: Container(
+        margin: EdgeInsetsDirectional.only(
+          start: AppConstants.mainButtonHorizontalMarginVal.w,
+          end: AppConstants.mainButtonHorizontalMarginVal.w,
+          top: 32.h,
+          bottom: 40.h,
+        ),
+        child: Text(
+          context.translate(LangKeys.howDoUWantToPay),
+          style: AppTextStyles.textStyle16Medium.copyWith(
+            color: AppColors.primaryColor,
+          ),
         ),
       ),
     );
