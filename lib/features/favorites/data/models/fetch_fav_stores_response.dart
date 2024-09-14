2b80c049b707180d3e5951b208bbec43176246ce
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-09-15 02:59:31 +0300

    Integrated Hive in favorites feature's models

--- lib/features/favorites/data/models/fetch_fav_stores_response.dart
+++ lib/features/favorites/data/models/fetch_fav_stores_response.dart
@@ -1,10 +1,14 @@
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:json_annotation/json_annotation.dart';
+import 'package:store_ify/core/helpers/hive_type_ids.dart';
 import 'package:store_ify/features/stores/data/models/store.dart';
 
 part 'fetch_fav_stores_response.g.dart';
 
+@HiveType(typeId: HiveTypeIds.fetchFavStoresResponse)
 @JsonSerializable()
 class FetchFavStoresResponse {
+  @HiveField(0)
   final List<Store> stores;
 
   const FetchFavStoresResponse({required this.stores});
--- lib/features/favorites/data/models/fetch_favorite_products_response.dart
+++ lib/features/favorites/data/models/fetch_favorite_products_response.dart
@@ -1,10 +1,14 @@
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:json_annotation/json_annotation.dart';
+import 'package:store_ify/core/helpers/hive_type_ids.dart';
 import 'package:store_ify/features/home/data/models/product.dart';
 
 part 'fetch_favorite_products_response.g.dart';
 
+@HiveType(typeId: HiveTypeIds.fetchFavoriteProductsResponse)
 @JsonSerializable()
 class FetchFavoriteProductsResponse {
+  @HiveField(0)
   final List<Product> products;
 
   const FetchFavoriteProductsResponse({required this.products});
