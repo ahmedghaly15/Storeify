e6543e9cb3504f99a9e2e925b1518c7f0ed46ff7
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-09-15 00:25:19 +0300

    Registered new Hive adapters & Added new HiveTypeIds

--- lib/core/helpers/hive_helper.dart
+++ lib/core/helpers/hive_helper.dart
@@ -1,6 +1,11 @@
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:store_ify/core/models/pagination.dart';
 import 'package:store_ify/core/models/storeify_user.dart';
+import 'package:store_ify/features/categories/data/models/category.dart';
 import 'package:store_ify/features/home/data/models/fetch_home_response.dart';
+import 'package:store_ify/features/home/data/models/product.dart';
+import 'package:store_ify/features/home/data/models/sub_category.dart';
+import 'package:store_ify/features/stores/data/models/store.dart';
 
 class HiveHelper {
   HiveHelper._();
@@ -9,5 +14,13 @@ class HiveHelper {
     Hive.registerAdapter(StoreifyUserAdapter());
     Hive.registerAdapter(UserDataAdapter());
     Hive.registerAdapter(FetchHomeResponseAdapter());
+    Hive.registerAdapter(ProductAdapter());
+    Hive.registerAdapter(ProductImgAdapter());
+    Hive.registerAdapter(ProductColorAdapter());
+    Hive.registerAdapter(ProductSizeAdapter());
+    Hive.registerAdapter(StoreAdapter());
+    Hive.registerAdapter(CategoryAdapter());
+    Hive.registerAdapter(SubCategoryAdapter());
+    Hive.registerAdapter(PaginationAdapter());
   }
 }
--- lib/core/helpers/hive_type_ids.dart
+++ lib/core/helpers/hive_type_ids.dart
@@ -4,4 +4,12 @@ class HiveTypeIds {
   static const int storeifyUser = 0;
   static const int storeifyUserData = 1;
   static const int fetchHomeResponse = 2;
+  static const int product = 3;
+  static const int productImg = 4;
+  static const int productColor = 5;
+  static const int productSize = 6;
+  static const int store = 7;
+  static const int category = 8;
+  static const int subCategory = 9;
+  static const int pagination = 10;
 }
