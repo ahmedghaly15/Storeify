0fcac3ac97283a1a334bcb8d4ab1df0f33ccf454
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-09-14 15:07:59 +0300

    Refactored CheckoutFormBlocBuilder widget

--- lib/features/checkout/presentation/widgets/checkout_form_bloc_builder.dart
+++ lib/features/checkout/presentation/widgets/checkout_form_bloc_builder.dart
@@ -7,9 +7,11 @@ import 'package:intl_phone_field/intl_phone_field.dart';
 import 'package:store_ify/core/helpers/auth_validator.dart';
 import 'package:store_ify/core/helpers/payment_validator.dart';
 import 'package:store_ify/core/locale/lang_keys.dart';
+import 'package:store_ify/core/services/location_service.dart';
 import 'package:store_ify/core/themes/app_colors.dart';
 import 'package:store_ify/core/utils/app_constants.dart';
 import 'package:store_ify/core/widgets/custom_circular_progress_indicator.dart';
+import 'package:store_ify/core/widgets/custom_error_widget.dart';
 import 'package:store_ify/core/widgets/custom_text_field.dart';
 import 'package:store_ify/core/widgets/my_sized_box.dart';
 import 'package:store_ify/features/auth/presentation/widgets/text_field_label.dart';
@@ -24,92 +26,111 @@ class CheckoutFormBlocBuilder extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<CheckoutCubit, CheckoutState>(
-      buildWhen: (_, current) => current is GetCurrentCountryCode,
-      builder: (context, state) => state.maybeWhen(
-        getCurrentCountryCode: (countryCode) => Form(
-          key: context.read<CheckoutCubit>().formKey,
-          child: FadeInDown(
-            from: 50.h,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const TextFieldLabel(labelKey: LangKeys.username),
-                CustomTextField(
-                  controller: context.read<CheckoutCubit>().usernameController,
-                  keyboardType: TextInputType.name,
-                  textCapitalization: TextCapitalization.none,
-                  hintTextKey: LangKeys.enterYourUsername,
-                  autofillHints: const <String>[AutofillHints.name],
-                  validate: (String? value) =>
-                      AuthValidator.validateNameField(context, value: value),
-                ),
-                MySizedBox.height10,
-                const TextFieldLabel(labelKey: LangKeys.yourAddress),
-                CustomTextField(
-                  controller: context.read<CheckoutCubit>().addressController,
-                  keyboardType: TextInputType.streetAddress,
-                  textCapitalization: TextCapitalization.sentences,
-                  hintTextKey: LangKeys.enterYourAddress,
-                  autofillHints: const <String>[AutofillHints.addressCity],
-                  validate: (String? value) =>
-                      PaymentValidator.validateField(context, value),
-                ),
-                MySizedBox.height10,
-                const TextFieldLabel(labelKey: LangKeys.phone),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 24.w),
-                  child: IntlPhoneField(
-                    initialCountryCode: countryCode,
-                    keyboardType: TextInputType.phone,
-                    pickerDialogStyle: PickerDialogStyle(
-                      backgroundColor: Colors.white,
-                    ),
-                    decoration: InputDecoration(
-                      enabledBorder: AppConstants.textFieldOutlinedBorder,
-                      focusedBorder: AppConstants.textFieldOutlinedBorder,
-                      errorBorder: AppConstants.textFieldOutlinedBorder,
-                      focusedErrorBorder: AppConstants.textFieldOutlinedBorder,
-                      border: AppConstants.textFieldOutlinedBorder,
-                      filled: true,
-                      fillColor: Colors.white,
-                    ),
-                    onChanged: (phoneNumber) => context
-                        .read<CheckoutCubit>()
-                        .onCountryChanged(
-                            '${phoneNumber.countryCode}${phoneNumber.number}'),
-                  ),
-                ),
-                MySizedBox.height10,
-                const TextFieldLabel(labelKey: LangKeys.date),
-                InkWell(
-                  onTap: () => context.read<CheckoutCubit>().pickDate(context),
-                  child: CustomTextField(
-                    enabled: false,
-                    controller: context.read<CheckoutCubit>().dateController,
-                    keyboardType: TextInputType.datetime,
+      buildWhen: (_, current) =>
+          current is CheckLocationPermission ||
+          current is LocationPermissionDenied ||
+          current is GetCurrentCountryCode,
+      builder: (context, state) {
+        if (state is GetCurrentCountryCode) {
+          return Form(
+            key: context.read<CheckoutCubit>().formKey,
+            child: FadeInDown(
+              from: 50.h,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const TextFieldLabel(labelKey: LangKeys.username),
+                  CustomTextField(
+                    controller:
+                        context.read<CheckoutCubit>().usernameController,
+                    keyboardType: TextInputType.name,
                     textCapitalization: TextCapitalization.none,
-                    hintTextKey: LangKeys.dateHint,
-                    autofillHints: const <String>[AutofillHints.birthday],
+                    hintTextKey: LangKeys.enterYourUsername,
+                    autofillHints: const <String>[AutofillHints.name],
+                    validate: (String? value) =>
+                        AuthValidator.validateNameField(context, value: value),
+                  ),
+                  MySizedBox.height10,
+                  const TextFieldLabel(labelKey: LangKeys.yourAddress),
+                  CustomTextField(
+                    controller: context.read<CheckoutCubit>().addressController,
+                    keyboardType: TextInputType.streetAddress,
+                    textCapitalization: TextCapitalization.sentences,
+                    hintTextKey: LangKeys.enterYourAddress,
+                    autofillHints: const <String>[AutofillHints.addressCity],
                     validate: (String? value) =>
                         PaymentValidator.validateField(context, value),
-                    suffixIcon: const Icon(
-                      Icons.calendar_month,
-                      color: AppColors.primaryColor,
+                  ),
+                  MySizedBox.height10,
+                  const TextFieldLabel(labelKey: LangKeys.phone),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 24.w),
+                    child: IntlPhoneField(
+                      initialCountryCode: state.countryCode,
+                      keyboardType: TextInputType.phone,
+                      pickerDialogStyle: PickerDialogStyle(
+                        backgroundColor: Colors.white,
+                      ),
+                      decoration: InputDecoration(
+                        enabledBorder: AppConstants.textFieldOutlinedBorder,
+                        focusedBorder: AppConstants.textFieldOutlinedBorder,
+                        errorBorder: AppConstants.textFieldOutlinedBorder,
+                        focusedErrorBorder:
+                            AppConstants.textFieldOutlinedBorder,
+                        border: AppConstants.textFieldOutlinedBorder,
+                        filled: true,
+                        fillColor: Colors.white,
+                      ),
+                      onChanged: (phoneNumber) => context
+                          .read<CheckoutCubit>()
+                          .onCountryChanged(
+                              '${phoneNumber.countryCode}${phoneNumber.number}'),
                     ),
                   ),
-                ),
-                MySizedBox.height10,
-                const TextFieldLabel(labelKey: LangKeys.time),
-                const CustomTimePicker(),
-                const CheckoutNextBlocConsumerButton(),
-              ],
+                  MySizedBox.height10,
+                  const TextFieldLabel(labelKey: LangKeys.date),
+                  InkWell(
+                    onTap: () =>
+                        context.read<CheckoutCubit>().pickDate(context),
+                    child: CustomTextField(
+                      enabled: false,
+                      controller: context.read<CheckoutCubit>().dateController,
+                      keyboardType: TextInputType.datetime,
+                      textCapitalization: TextCapitalization.none,
+                      hintTextKey: LangKeys.dateHint,
+                      autofillHints: const <String>[AutofillHints.birthday],
+                      validate: (String? value) =>
+                          PaymentValidator.validateField(context, value),
+                      suffixIcon: const Icon(
+                        Icons.calendar_month,
+                        color: AppColors.primaryColor,
+                      ),
+                    ),
+                  ),
+                  MySizedBox.height10,
+                  const TextFieldLabel(labelKey: LangKeys.time),
+                  const CustomTimePicker(),
+                  const CheckoutNextBlocConsumerButton(),
+                ],
+              ),
             ),
-          ),
-        ),
-        orElse: () => const Center(
-          child: CustomCircularProgressIndicator(),
-        ),
-      ),
+          );
+        } else if (state is LocationPermissionDenied) {
+          return CustomErrorWidget(
+            tryAgainOnPressed: () async {
+              await LocationService.requestPermission();
+              context
+                  .read<CheckoutCubit>()
+                  .checkLocationPermissionAndGetCountryCode();
+            },
+            errorKey: state.error,
+          );
+        } else {
+          return const Center(
+            child: CustomCircularProgressIndicator(),
+          );
+        }
+      },
     );
   }
 }
