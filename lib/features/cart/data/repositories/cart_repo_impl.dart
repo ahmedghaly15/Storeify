8bb3bb0dda2728baed2942020cac7f0a1ae054ed
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-09-15 00:55:02 +0300

    Handled caching cart

--- lib/features/cart/data/repositories/cart_repo_impl.dart
+++ lib/features/cart/data/repositories/cart_repo_impl.dart
@@ -1,7 +1,10 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:store_ify/core/api/api_error_handler.dart';
 import 'package:store_ify/core/api/api_result.dart';
 import 'package:store_ify/core/api/api_service.dart';
 import 'package:store_ify/core/utils/functions/execute_and_handle_errors.dart';
+import 'package:store_ify/features/cart/data/datasources/cart_local_datasource.dart';
 import 'package:store_ify/features/cart/data/models/add_product_to_cart_params.dart';
 import 'package:store_ify/features/cart/data/models/fetch_cart_response.dart';
 import 'package:store_ify/features/cart/data/repositories/cart_repo.dart';
@@ -28,10 +31,23 @@ class CartRepoImpl implements CartRepo {
   @override
   Future<ApiResult<FetchCartResponse>> fetchCart([
     CancelToken? cancelToken,
-  ]) {
-    return executeAndHandleErrors<FetchCartResponse>(
-      () async => await _apiService.fetchCart(cancelToken),
-    );
+  ]) async {
+    final FetchCartResponse? cachedCart =
+        await CartLocalDatasource.retrieveCachedCart();
+    if (cachedCart == null) {
+      debugPrint('*********** NO CACHED CART ***********');
+      try {
+        final cart = await _apiService.fetchCart(cancelToken);
+        await CartLocalDatasource.cacheCart(cart);
+        return ApiResult.success(cart);
+      } catch (error) {
+        debugPrint('********* ERROR FETCHING CART: $error *********');
+        return ApiResult.error(ApiErrorHandler.handle(error));
+      }
+    } else {
+      debugPrint('*********** GOT CACHED CART ***********');
+      return ApiResult.success(cachedCart);
+    }
   }
 
   @override
