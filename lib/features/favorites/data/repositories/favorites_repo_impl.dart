8288bd98fbbfc85100c8be509dcd9f6d052e8d6e
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-09-15 17:56:59 +0300

    Handled caching fav products, stores in FavoritesRepoImpl

--- lib/features/favorites/data/repositories/favorites_repo_impl.dart
+++ lib/features/favorites/data/repositories/favorites_repo_impl.dart
@@ -1,16 +1,20 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/widgets.dart';
+import 'package:store_ify/core/api/api_error_handler.dart';
 import 'package:store_ify/core/api/api_result.dart';
 import 'package:store_ify/core/api/api_service.dart';
 import 'package:store_ify/core/utils/functions/execute_and_handle_errors.dart';
+import 'package:store_ify/features/favorites/data/datasources/favorites_local_datasource.dart';
 import 'package:store_ify/features/favorites/data/models/fetch_fav_stores_response.dart';
 import 'package:store_ify/features/favorites/data/models/fetch_favorite_products_response.dart';
 import 'package:store_ify/features/favorites/data/models/prefer_params.dart';
 import 'package:store_ify/features/favorites/data/repositories/favorites_repo.dart';
 
 class FavoritesRepoImpl implements FavoritesRepo {
-  const FavoritesRepoImpl(this._apiService);
-
   final ApiService _apiService;
+  final FavoritesLocalDatasource _localDatasource;
+
+  const FavoritesRepoImpl(this._apiService, this._localDatasource);
 
   @override
   Future<ApiResult<void>> preferProduct(
@@ -41,19 +45,58 @@ class FavoritesRepoImpl implements FavoritesRepo {
   @override
   Future<ApiResult<FetchFavoriteProductsResponse>> fetchFavoriteProducts([
     CancelToken? cancelToken,
-  ]) {
-    return executeAndHandleErrors<FetchFavoriteProductsResponse>(
-      () async => await _apiService.fetchFavoriteProducts(cancelToken),
-    );
+  ]) async {
+    final FetchFavoriteProductsResponse? cachedFavProducts =
+        await _localDatasource.retrieveCachedFavProducts();
+    if (cachedFavProducts == null) {
+      debugPrint('********** CACHED FAV PRODUCTS NOT FOUND **********');
+      return await _fetchAndCacheFavProducts(cancelToken);
+    } else {
+      debugPrint('********** GOT CACHED FAV PRODUCTS **********');
+      return ApiResult.success(cachedFavProducts);
+    }
+  }
+
+  Future<ApiResult<FetchFavoriteProductsResponse>> _fetchAndCacheFavProducts(
+    CancelToken? cancelToken,
+  ) async {
+    try {
+      final FetchFavoriteProductsResponse favProducts =
+          await _apiService.fetchFavoriteProducts(cancelToken);
+      await _localDatasource.cacheFetchedFavProducts(favProducts);
+      return ApiResult.success(favProducts);
+    } catch (error) {
+      debugPrint('********** ERROR FETCHING FAV PRODUCTS: $error **********');
+      return ApiResult.error(ApiErrorHandler.handle(error));
+    }
   }
 
   @override
   Future<ApiResult<FetchFavStoresResponse>> fetchFavStores([
     CancelToken? cancelToken,
-  ]) {
-    return executeAndHandleErrors<FetchFavStoresResponse>(
-      () async => await _apiService.fetchFavStores(cancelToken),
-    );
+  ]) async {
+    final FetchFavStoresResponse? cachedFavStores =
+        await _localDatasource.retrieveCachedFavStores();
+    if (cachedFavStores == null) {
+      debugPrint('********** CACHED FAV STORES NOT FOUND **********');
+      return await _fetchAndCacheFavStores(cancelToken);
+    } else {
+      debugPrint('********** GOT CACHED FAV STORES **********');
+      return ApiResult.success(cachedFavStores);
+    }
+  }
+
+  Future<ApiResult<FetchFavStoresResponse>> _fetchAndCacheFavStores(
+      CancelToken? cancelToken) async {
+    try {
+      final FetchFavStoresResponse favStores =
+          await _apiService.fetchFavStores(cancelToken);
+      await _localDatasource.cacheFetchedFavStores(favStores);
+      return ApiResult.success(favStores);
+    } catch (error) {
+      debugPrint('********** ERROR FETCHING FAV STORES: $error **********');
+      return ApiResult.error(ApiErrorHandler.handle(error));
+    }
   }
 
   @override
