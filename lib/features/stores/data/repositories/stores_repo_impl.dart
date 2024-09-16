07c7d4b5aee75a9836c10ab2364dee9481ef26bb
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-09-16 15:15:55 +0300

    Refactored fetchStores & Built _fetchAndCacheStores methods in StoresRepoImpl

--- lib/features/stores/data/repositories/stores_repo_impl.dart
+++ lib/features/stores/data/repositories/stores_repo_impl.dart
@@ -1,7 +1,10 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:store_ify/core/api/api_error_handler.dart';
 import 'package:store_ify/core/api/api_result.dart';
 import 'package:store_ify/core/api/api_service.dart';
 import 'package:store_ify/core/utils/functions/execute_and_handle_errors.dart';
+import 'package:store_ify/features/stores/data/datasources/stores_local_datasource.dart';
 import 'package:store_ify/features/stores/data/models/fetch_store_branches.dart';
 import 'package:store_ify/features/stores/data/models/fetch_store_categories_response.dart';
 import 'package:store_ify/features/stores/data/models/fetch_store_offers_response.dart';
@@ -9,17 +12,37 @@ import 'package:store_ify/features/stores/data/models/fetch_stores_response.dart
 import 'package:store_ify/features/stores/data/repositories/stores_repo.dart';
 
 class StoresRepoImpl implements StoresRepo {
-  const StoresRepoImpl(this._apiService);
-
   final ApiService _apiService;
+  final StoresLocalDatasource _localDatasource;
+
+  const StoresRepoImpl(this._apiService, this._localDatasource);
 
   @override
   Future<ApiResult<FetchStoresResponse>> fetchStores([
     CancelToken? cancelToken,
-  ]) {
-    return executeAndHandleErrors<FetchStoresResponse>(
-      () async => await _apiService.fetchStores(cancelToken),
-    );
+  ]) async {
+    final FetchStoresResponse? cachedStores =
+        await _localDatasource.retrieveFetchedStores();
+    if (cachedStores == null) {
+      debugPrint('*********** NO CACHED STORES ***********');
+      return await _fetchAndCacheStores(cancelToken);
+    } else {
+      debugPrint('*********** GOT CACHED STORES ***********');
+      return ApiResult.success(cachedStores);
+    }
+  }
+
+  Future<ApiResult<FetchStoresResponse>> _fetchAndCacheStores(
+    CancelToken? cancelToken,
+  ) async {
+    try {
+      final stores = await _apiService.fetchStores(cancelToken);
+      await _localDatasource.cacheFetchedStores(stores);
+      return ApiResult.success(stores);
+    } catch (error) {
+      debugPrint('****** ERROR FETCHING CACHED STORES: $error ******');
+      return ApiResult.error(ApiErrorHandler.handle(error));
+    }
   }
 
   @override
