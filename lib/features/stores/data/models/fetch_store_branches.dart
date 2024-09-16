5e33332d630c1821bf7501aca0cc893963ffed5d
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-09-16 15:35:53 +0300

    Integrated Hive in stores feature's models

--- lib/features/stores/data/models/fetch_store_branches.dart
+++ lib/features/stores/data/models/fetch_store_branches.dart
@@ -1,9 +1,13 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:store_ify/core/helpers/hive_type_ids.dart';
 
 part 'fetch_store_branches.g.dart';
 
+@HiveType(typeId: HiveTypeIds.fetchStoreBranchesResponse)
 @JsonSerializable()
 class FetchStoreBranchesResponse {
+  @HiveField(0)
   final List<StoreBranch> branches;
 
   const FetchStoreBranchesResponse({required this.branches});
@@ -13,10 +17,15 @@ class FetchStoreBranchesResponse {
   Map<String, dynamic> toJson() => _$FetchStoreBranchesResponseToJson(this);
 }
 
+@HiveType(typeId: HiveTypeIds.storeBranch)
 @JsonSerializable()
 class StoreBranch {
+  @HiveField(0)
   final int id;
-  final String name, address;
+  @HiveField(1)
+  final String name;
+  @HiveField(2)
+  final String address;
 
   const StoreBranch({
     required this.id,
--- lib/features/stores/data/models/fetch_store_branches.g.dart
+++ lib/features/stores/data/models/fetch_store_branches.g.dart
@@ -3,6 +3,85 @@
 part of 'fetch_store_branches.dart';
 
 // **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class FetchStoreBranchesResponseAdapter
+    extends TypeAdapter<FetchStoreBranchesResponse> {
+  @override
+  final int typeId = 18;
+
+  @override
+  FetchStoreBranchesResponse read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return FetchStoreBranchesResponse(
+      branches: (fields[0] as List).cast<StoreBranch>(),
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, FetchStoreBranchesResponse obj) {
+    writer
+      ..writeByte(1)
+      ..writeByte(0)
+      ..write(obj.branches);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is FetchStoreBranchesResponseAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+class StoreBranchAdapter extends TypeAdapter<StoreBranch> {
+  @override
+  final int typeId = 21;
+
+  @override
+  StoreBranch read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return StoreBranch(
+      id: fields[0] as int,
+      name: fields[1] as String,
+      address: fields[2] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, StoreBranch obj) {
+    writer
+      ..writeByte(3)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.name)
+      ..writeByte(2)
+      ..write(obj.address);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is StoreBranchAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+// **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
 
