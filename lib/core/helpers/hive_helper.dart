12bb1aa8f03d392a289e9e21f7123ac004b1d89d
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-09-16 14:50:57 +0300

    Added clearAll() to HiveHelper

--- lib/core/helpers/hive_helper.dart
+++ lib/core/helpers/hive_helper.dart
@@ -1,8 +1,11 @@
+import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:store_ify/core/helpers/hive_boxes.dart';
+import 'package:store_ify/core/models/color.dart';
 import 'package:store_ify/core/models/pagination.dart';
+import 'package:store_ify/core/models/size.dart';
 import 'package:store_ify/core/models/storeify_user.dart';
 import 'package:store_ify/features/cart/data/models/cart.dart';
-import 'package:store_ify/features/cart/data/models/cart_product.dart';
 import 'package:store_ify/features/cart/data/models/fetch_cart_response.dart';
 import 'package:store_ify/features/categories/data/models/category.dart';
 import 'package:store_ify/features/categories/data/models/fetch_categories_response.dart';
@@ -23,18 +26,49 @@ class HiveHelper {
     Hive.registerAdapter(FetchHomeResponseAdapter());
     Hive.registerAdapter(ProductAdapter());
     Hive.registerAdapter(ProductImgAdapter());
-    Hive.registerAdapter(ProductColorAdapter());
-    Hive.registerAdapter(ProductSizeAdapter());
+    Hive.registerAdapter(ColorAdapter());
+    Hive.registerAdapter(SizeAdapter());
     Hive.registerAdapter(StoreAdapter());
     Hive.registerAdapter(CategoryAdapter());
     Hive.registerAdapter(SubCategoryAdapter());
     Hive.registerAdapter(PaginationAdapter());
-    Hive.registerAdapter(CartProductAdapter());
     Hive.registerAdapter(CartAdapter());
     Hive.registerAdapter(FetchCartResponseAdapter());
     Hive.registerAdapter(FetchSubCategoryResponseAdapter());
     Hive.registerAdapter(FetchCategoriesResponseAdapter());
     Hive.registerAdapter(FetchFavStoresResponseAdapter());
     Hive.registerAdapter(FetchFavoriteProductsResponseAdapter());
+  }
+
+  static Future<void> clearAllBoxes() async {
+    final homeResponseBox = await Hive.openBox<FetchHomeResponse>(
+      HiveBoxes.homeResponseBox,
+    );
+    final cartResponseBox = await Hive.openBox<FetchCartResponse>(
+      HiveBoxes.cartResponseBox,
+    );
+    final categoriesResponseBox = await Hive.openBox<FetchCategoriesResponse>(
+      HiveBoxes.categoriesResponseBox,
+    );
+    final subCategoryBox = await Hive.openBox<FetchSubCategoryResponse>(
+      HiveBoxes.subCategoryBox,
+    );
+    final favProductsBox = await Hive.openBox<FetchFavoriteProductsResponse>(
+      HiveBoxes.favProductsBox,
+    );
+    final favStoresBox = await Hive.openBox<FetchFavStoresResponse>(
+      HiveBoxes.favStoresBox,
+    );
+
+    debugPrint('********** ALL BOXES CLEARED **********');
+
+    await Future.wait([
+      homeResponseBox.clear(),
+      cartResponseBox.clear(),
+      categoriesResponseBox.clear(),
+      subCategoryBox.clear(),
+      favProductsBox.clear(),
+      favStoresBox.clear(),
+    ]);
   }
 }
--- lib/core/helpers/hive_type_ids.dart
+++ lib/core/helpers/hive_type_ids.dart
@@ -6,13 +6,12 @@ class HiveTypeIds {
   static const int fetchHomeResponse = 2;
   static const int product = 3;
   static const int productImg = 4;
-  static const int productColor = 5;
-  static const int productSize = 6;
+  static const int color = 5;
+  static const int size = 6;
   static const int store = 7;
   static const int category = 8;
   static const int subCategory = 9;
   static const int pagination = 10;
-  static const int cartProduct = 11;
   static const int cart = 12;
   static const int fetchCartResponse = 13;
   static const int fetchCategoriesResponse = 14;
