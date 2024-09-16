f367018619dc35bbdd59028aff93607df16a61e0
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-09-16 14:48:11 +0300

    Updated Cart model & Deleted CartProduct model

--- lib/features/cart/data/models/cart.dart
+++ lib/features/cart/data/models/cart.dart
@@ -1,7 +1,9 @@
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:store_ify/core/helpers/hive_type_ids.dart';
-import 'package:store_ify/features/cart/data/models/cart_product.dart';
+import 'package:store_ify/core/models/color.dart';
+import 'package:store_ify/core/models/size.dart';
+import 'package:store_ify/features/home/data/models/product.dart';
 
 part 'cart.g.dart';
 
@@ -17,38 +19,24 @@ class Cart {
   @HiveField(3)
   final double total;
   @HiveField(4)
-  @JsonKey(name: 'size_id')
-  final int sizeId;
-  @HiveField(5)
-  @JsonKey(name: 'color_id')
-  final int colorId;
-  @HiveField(6)
   @JsonKey(name: 'cart_id')
   final int cartId;
+  @HiveField(5)
+  final Product product;
+  @HiveField(6)
+  final Color color;
   @HiveField(7)
-  @JsonKey(name: 'product_id')
-  final int productId;
-  @HiveField(8)
-  @JsonKey(name: 'created_at')
-  final String createdAt;
-  @HiveField(9)
-  @JsonKey(name: 'updated_at')
-  final String updatedAt;
-  @HiveField(10)
-  final CartProduct product;
+  final Size size;
 
   const Cart({
     required this.id,
     required this.quantity,
-    required this.sizeId,
-    required this.colorId,
     required this.cartId,
-    required this.productId,
-    required this.createdAt,
-    required this.updatedAt,
     required this.product,
     required this.price,
     required this.total,
+    required this.color,
+    required this.size,
   });
 
   factory Cart.fromJson(Map<String, dynamic> json) => _$CartFromJson(json);
--- lib/features/cart/data/models/cart.g.dart
+++ lib/features/cart/data/models/cart.g.dart
@@ -19,22 +19,19 @@ class CartAdapter extends TypeAdapter<Cart> {
     return Cart(
       id: fields[0] as int,
       quantity: fields[1] as int,
-      sizeId: fields[4] as int,
-      colorId: fields[5] as int,
-      cartId: fields[6] as int,
-      productId: fields[7] as int,
-      createdAt: fields[8] as String,
-      updatedAt: fields[9] as String,
-      product: fields[10] as CartProduct,
+      cartId: fields[4] as int,
+      product: fields[5] as Product,
       price: fields[2] as double,
       total: fields[3] as double,
+      color: fields[6] as Color,
+      size: fields[7] as Size,
     );
   }
 
   @override
   void write(BinaryWriter writer, Cart obj) {
     writer
-      ..writeByte(11)
+      ..writeByte(8)
       ..writeByte(0)
       ..write(obj.id)
       ..writeByte(1)
@@ -44,19 +41,13 @@ class CartAdapter extends TypeAdapter<Cart> {
       ..writeByte(3)
       ..write(obj.total)
       ..writeByte(4)
-      ..write(obj.sizeId)
-      ..writeByte(5)
-      ..write(obj.colorId)
-      ..writeByte(6)
       ..write(obj.cartId)
+      ..writeByte(5)
+      ..write(obj.product)
+      ..writeByte(6)
+      ..write(obj.color)
       ..writeByte(7)
-      ..write(obj.productId)
-      ..writeByte(8)
-      ..write(obj.createdAt)
-      ..writeByte(9)
-      ..write(obj.updatedAt)
-      ..writeByte(10)
-      ..write(obj.product);
+      ..write(obj.size);
   }
 
   @override
@@ -77,15 +68,12 @@ class CartAdapter extends TypeAdapter<Cart> {
 Cart _$CartFromJson(Map<String, dynamic> json) => Cart(
       id: (json['id'] as num).toInt(),
       quantity: (json['quantity'] as num).toInt(),
-      sizeId: (json['size_id'] as num).toInt(),
-      colorId: (json['color_id'] as num).toInt(),
       cartId: (json['cart_id'] as num).toInt(),
-      productId: (json['product_id'] as num).toInt(),
-      createdAt: json['created_at'] as String,
-      updatedAt: json['updated_at'] as String,
-      product: CartProduct.fromJson(json['product'] as Map<String, dynamic>),
+      product: Product.fromJson(json['product'] as Map<String, dynamic>),
       price: (json['price'] as num).toDouble(),
       total: (json['total'] as num).toDouble(),
+      color: Color.fromJson(json['color'] as Map<String, dynamic>),
+      size: Size.fromJson(json['size'] as Map<String, dynamic>),
     );
 
 Map<String, dynamic> _$CartToJson(Cart instance) => <String, dynamic>{
@@ -93,11 +81,8 @@ Map<String, dynamic> _$CartToJson(Cart instance) => <String, dynamic>{
       'quantity': instance.quantity,
       'price': instance.price,
       'total': instance.total,
-      'size_id': instance.sizeId,
-      'color_id': instance.colorId,
       'cart_id': instance.cartId,
-      'product_id': instance.productId,
-      'created_at': instance.createdAt,
-      'updated_at': instance.updatedAt,
       'product': instance.product,
+      'color': instance.color,
+      'size': instance.size,
     };
