871581f122df2383f04567e55204edd7b2a7502f
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-09-17 11:57:12 +0300

    Updated CustomSearchTextField & GetBackIconButton widgets

--- lib/core/widgets/get_back_icon_button.dart
+++ lib/core/widgets/get_back_icon_button.dart
@@ -2,12 +2,14 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
 class GetBackIconButton extends StatelessWidget {
-  const GetBackIconButton({super.key});
+  const GetBackIconButton({super.key, this.onTap});
+
+  final VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
     return IconButton(
-      onPressed: () => context.maybePop(),
+      onPressed: onTap ?? () => context.maybePop(),
       icon: Icon(
         Icons.arrow_back,
         color: const Color(0xff525252).withOpacity(0.58),
--- lib/features/home/presentation/widgets/custom_search_text_field.dart
+++ lib/features/home/presentation/widgets/custom_search_text_field.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:store_ify/core/locale/lang_keys.dart';
 import 'package:store_ify/core/utils/app_assets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:store_ify/core/utils/app_constants.dart';
 import 'package:store_ify/core/widgets/custom_text_field.dart';
 
 class CustomSearchTextField extends StatelessWidget {
@@ -9,35 +10,29 @@ class CustomSearchTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(24.r),
-        boxShadow: <BoxShadow>[
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.2),
-            spreadRadius: 0,
-            blurRadius: 10.h,
-            offset: Offset(0, 3.h),
-          ),
-        ],
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(24.r),
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.2),
+              spreadRadius: 0,
+              blurRadius: 10.h,
+              offset: Offset(0, 3.h),
+            ),
+          ],
+        ),
+        child: CustomTextField(
+          enabled: false,
+          hintTextKey: LangKeys.search,
+          prefixIcon: Image.asset(AppAssets.iconsSearch),
+          enabledBorder: AppConstants.searchFieldOutlineInputBorder,
+          focusedBorder: AppConstants.searchFieldOutlineInputBorder,
+        ),
       ),
-      child: CustomTextField(
-        hintTextKey: LangKeys.search,
-        prefixIcon: Image.asset(AppAssets.iconsSearch),
-        onChanged: (String val) {},
-        textAlign: TextAlign.start,
-        keyboardType: TextInputType.text,
-        enabledBorder: _outlineInputBorder(),
-        focusedBorder: _outlineInputBorder(),
-      ),
-    );
-  }
-
-  OutlineInputBorder _outlineInputBorder() {
-    return OutlineInputBorder(
-      borderRadius: BorderRadius.circular(24.r),
-      borderSide: const BorderSide(color: Colors.white),
     );
   }
 }
