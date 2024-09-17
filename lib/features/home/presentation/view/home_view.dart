ec469501f55ead6db02a7148b1e53544e24f192d
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-09-17 12:42:33 +0300

    Updated HomeView & Setuped DI for Search Repo, Cubit  - Wrapped CustomSearchTextField with a Hero widget

--- lib/features/home/presentation/view/home_view.dart
+++ lib/features/home/presentation/view/home_view.dart
@@ -1,11 +1,11 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:store_ify/core/locale/lang_keys.dart';
 import 'package:store_ify/core/utils/app_constants.dart';
 import 'package:store_ify/dependency_injection.dart';
 import 'package:store_ify/features/home/presentation/cubit/home_cubit.dart';
-import 'package:store_ify/features/home/presentation/widgets/custom_search_text_field.dart';
+import 'package:store_ify/features/search/presentation/widgets/custom_search_text_field.dart';
 import 'package:store_ify/features/home/presentation/widgets/home_data_bloc_builder.dart';
 import 'package:store_ify/features/home/presentation/widgets/show_location_dialog_bloc_listener.dart';
 import 'package:store_ify/features/home/presentation/widgets/home_custom_app_bar.dart';
@@ -35,12 +35,12 @@ class HomeView extends StatelessWidget implements AutoRouteWrapper {
             child: HomeCustomAppBar(),
           ),
           SliverToBoxAdapter(
-            child: Container(
-              margin: EdgeInsetsDirectional.symmetric(
-                horizontal: 16.w,
-                vertical: 16.h,
+            child: Hero(
+              tag: LangKeys.search,
+              child: GestureDetector(
+                onTap: () {},
+                child: const CustomSearchTextField(enabled: false),
               ),
-              child: const CustomSearchTextField(),
             ),
           ),
           const SliverFillRemaining(
