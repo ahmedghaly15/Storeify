8162ca923c1751b6fb4b7800191b5c649becb08a
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-09-18 04:37:56 +0300

    Built PreferStoreBlocListenerIconButton widget & Refactored StoreItem widget

---
+++ lib/core/widgets/prefer_store_bloc_listener_icon_button.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:store_ify/core/themes/app_colors.dart';
+import 'package:store_ify/core/widgets/custom_toast.dart';
+import 'package:store_ify/features/favorites/presentation/cubits/favorites/favorites_cubit.dart';
+import 'package:store_ify/features/favorites/presentation/cubits/favorites/favorites_state.dart';
+
+class PreferStoreBlocListenerIconButton extends StatelessWidget {
+  const PreferStoreBlocListenerIconButton({
+    super.key,
+    this.isFavorited = false,
+    required this.storeId,
+  });
+
+  final bool isFavorited;
+  final int storeId;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<FavoritesCubit, FavoritesState>(
+      listenWhen: (_, current) =>
+          current is PreferStoreError || current is RemoveStoreFromFavsError,
+      listener: (context, state) => state.whenOrNull(
+        preferStoreError: (errorKey) => CustomToast.showToast(
+          context: context,
+          messageKey: errorKey,
+          state: CustomToastState.error,
+        ),
+        removeStoreFromFavsError: (errorKey) => CustomToast.showToast(
+          context: context,
+          messageKey: errorKey,
+          state: CustomToastState.error,
+        ),
+      ),
+      child: IconButton(
+        onPressed: () => context.read<FavoritesCubit>().preferStoreOrNot(
+              storeId: storeId,
+              isFavorited: isFavorited,
+            ),
+        icon: Icon(
+          isFavorited ? Icons.favorite : Icons.favorite_border_outlined,
+          size: 19.w,
+          color: AppColors.primaryColor,
+        ),
+      ),
+    );
+  }
+}
--- lib/features/stores/presentation/widgets/store_item.dart
+++ lib/features/stores/presentation/widgets/store_item.dart
@@ -1,6 +1,5 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:store_ify/core/helpers/extensions.dart';
 import 'package:store_ify/core/router/app_router.dart';
 import 'package:store_ify/core/themes/app_colors.dart';
@@ -8,10 +7,8 @@ import 'package:store_ify/core/themes/app_text_styles.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:store_ify/core/utils/app_constants.dart';
 import 'package:store_ify/core/widgets/custom_cached_network_image.dart';
-import 'package:store_ify/core/widgets/custom_toast.dart';
 import 'package:store_ify/core/widgets/my_sized_box.dart';
-import 'package:store_ify/features/favorites/presentation/cubits/favorites/favorites_cubit.dart';
-import 'package:store_ify/features/favorites/presentation/cubits/favorites/favorites_state.dart';
+import 'package:store_ify/core/widgets/prefer_store_bloc_listener_icon_button.dart';
 import 'package:store_ify/features/stores/data/models/store.dart';
 
 class StoreItem extends StatelessWidget {
@@ -64,7 +61,7 @@ class StoreItem extends StatelessWidget {
                   maxLines: 1,
                   overflow: TextOverflow.ellipsis,
                 ),
-                PreferStoreBlocConsumerIconButton(
+                PreferStoreBlocListenerIconButton(
                   storeId: store.id,
                   // TODO:
                   // isFavorited: store.isFavorited,
@@ -72,48 +69,6 @@ class StoreItem extends StatelessWidget {
               ],
             ),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class PreferStoreBlocConsumerIconButton extends StatelessWidget {
-  const PreferStoreBlocConsumerIconButton({
-    super.key,
-    this.isFavorited = false,
-    required this.storeId,
-  });
-
-  final bool isFavorited;
-  final int storeId;
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocListener<FavoritesCubit, FavoritesState>(
-      listenWhen: (_, current) =>
-          current is PreferStoreError || current is RemoveStoreFromFavsError,
-      listener: (context, state) => state.whenOrNull(
-        preferStoreError: (errorKey) => CustomToast.showToast(
-          context: context,
-          messageKey: errorKey,
-          state: CustomToastState.error,
-        ),
-        removeStoreFromFavsError: (errorKey) => CustomToast.showToast(
-          context: context,
-          messageKey: errorKey,
-          state: CustomToastState.error,
-        ),
-      ),
-      child: IconButton(
-        onPressed: () => context.read<FavoritesCubit>().preferStoreOrNot(
-              storeId: storeId,
-              isFavorited: isFavorited,
-            ),
-        icon: Icon(
-          isFavorited ? Icons.favorite : Icons.favorite_border_outlined,
-          size: 19.w,
-          color: AppColors.primaryColor,
         ),
       ),
     );
