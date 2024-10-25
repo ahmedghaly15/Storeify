b6748ec681d7cb1708b189a18b459c2a612e5610
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-10-26 02:05:31 +0300

    Built CustomAdaptiveDialog

---
+++ lib/core/widgets/custom_adaptive_dialog.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:store_ify/core/helpers/extensions.dart';
+import 'package:store_ify/core/themes/app_colors.dart';
+import 'package:store_ify/core/themes/app_text_styles.dart';
+import 'package:store_ify/core/utils/app_assets.dart';
+
+class CustomAdaptiveDialog extends StatelessWidget {
+  const CustomAdaptiveDialog({
+    super.key,
+    this.actions,
+    this.content,
+    this.title,
+    this.contentText,
+    this.titleText,
+    this.contentPadding,
+    this.actionsPadding,
+  });
+
+  final List<Widget>? actions;
+  final Widget? content, title;
+  final String? contentText, titleText;
+  final EdgeInsetsGeometry? contentPadding, actionsPadding;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog.adaptive(
+      icon: Image.asset(
+        AppAssets.appIcon,
+        width: 48.w,
+        height: 48.h,
+      ),
+      backgroundColor: context.isDarkModeActive
+          ? AppColors.darkColor
+          : AppColors.lightModeColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16.r),
+      ),
+      title: title ??
+          (titleText != null
+              ? Text(
+                  context.translate(titleText!),
+                  style: AppTextStyles.textStyle20Medium,
+                )
+              : null),
+      content: content ??
+          Text(
+            context.translate(contentText!),
+            style: AppTextStyles.textStyle16Medium,
+          ),
+      actions: actions,
+      actionsPadding: actionsPadding,
+      contentPadding: contentPadding,
+    );
+  }
+}
