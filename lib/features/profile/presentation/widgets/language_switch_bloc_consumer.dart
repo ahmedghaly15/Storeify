1224cba8e73bdb73c6f1194124da5858ee959462
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-10-25 18:12:22 +0300

    Updated StoreifyMaterialApp & LanguageSwitchBlocConsumer widget  - Used BlocBuilder in StoreifyMaterialApp widget instead of BlocConsumer  - Updated listenWhen, listener in LanguageSwitchBlocConsumer widget

--- lib/features/profile/presentation/widgets/language_switch_bloc_consumer.dart
+++ lib/features/profile/presentation/widgets/language_switch_bloc_consumer.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:store_ify/core/locale/logic/cubit/locale_cubit.dart';
 import 'package:store_ify/core/locale/logic/cubit/locale_state.dart';
 import 'package:store_ify/core/widgets/custom_adaptive_switch.dart';
+import 'package:store_ify/core/widgets/custom_toast.dart';
 
 class LanguageSwitchBlocConsumer extends StatelessWidget {
   const LanguageSwitchBlocConsumer({super.key});
@@ -12,20 +13,35 @@ class LanguageSwitchBlocConsumer extends StatelessWidget {
   Widget build(BuildContext context) {
     final localeCubit = context.read<LocaleCubit>();
     return BlocConsumer<LocaleCubit, LocaleState>(
-      listenWhen: (_, current) =>
-          current.status == LocaleStateStatus.changeLocaleLocally,
-      listener: (context, state) {
-        switch (state.status) {
-          case LocaleStateStatus.changeLocaleLocally:
-            context.maybePop();
-            break;
-        }
-      },
+      listenWhen: (_, current) => _listenWhen(current),
+      listener: (context, state) => _localeCubitListener(state, context),
       buildWhen: (previous, current) => previous.locale != current.locale,
       builder: (context, state) => CustomAdaptiveSwitch(
         value: localeCubit.isArabic ? true : false,
         onChanged: (_) => localeCubit.toggleLocale(),
       ),
     );
+  }
+
+  bool _listenWhen(LocaleState<dynamic> current) {
+    return current.status == LocaleStateStatus.changeLocaleLocally ||
+        current.status == LocaleStateStatus.changeApiLocaleFailure;
+  }
+
+  void _localeCubitListener(LocaleState<dynamic> state, BuildContext context) {
+    switch (state.status) {
+      case LocaleStateStatus.changeLocaleLocally:
+        context.read<LocaleCubit>().changeApiLang(state.locale.languageCode);
+        context.maybePop();
+        break;
+
+      case LocaleStateStatus.changeApiLocaleFailure:
+        CustomToast.showToast(
+          context: context,
+          messageKey: state.error ?? '',
+          state: CustomToastState.error,
+        );
+        break;
+    }
   }
 }
--- lib/storeify_material_app.dart
+++ lib/storeify_material_app.dart
@@ -1,26 +1,22 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:store_ify/core/themes/app_themes.dart';
-import 'package:store_ify/core/utils/auto_route_observer.dart';
+
+import 'package:store_ify/core/di/dependency_injection.dart';
 import 'package:store_ify/core/locale/app_localizations_setup.dart';
 import 'package:store_ify/core/locale/logic/cubit/locale_cubit.dart';
 import 'package:store_ify/core/locale/logic/cubit/locale_state.dart';
 import 'package:store_ify/core/router/app_router.dart';
+import 'package:store_ify/core/themes/app_themes.dart';
 import 'package:store_ify/core/utils/app_strings.dart';
-import 'package:store_ify/core/di/dependency_injection.dart';
-import 'package:store_ify/core/widgets/custom_toast.dart';
+import 'package:store_ify/core/utils/auto_route_observer.dart';
 
 class StoreifyMaterialApp extends StatelessWidget {
   const StoreifyMaterialApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<LocaleCubit, LocaleState>(
-      listenWhen: (_, current) =>
-          current.status == LocaleStateStatus.changeLocaleLocally ||
-          current.status == LocaleStateStatus.changeApiLocaleFailure,
-      listener: (context, state) => _localeCubitListener(state, context),
+    return BlocBuilder<LocaleCubit, LocaleState>(
       buildWhen: (previous, current) => previous.locale != current.locale,
       builder: (_, localeState) => MaterialApp.router(
         debugShowCheckedModeBanner: false,
@@ -41,19 +37,4 @@ class StoreifyMaterialApp extends StatelessWidget {
     );
   }
 
-  void _localeCubitListener(LocaleState<dynamic> state, BuildContext context) {
-    switch (state.status) {
-      case LocaleStateStatus.changeLocaleLocally:
-        context.read<LocaleCubit>().changeApiLang(state.locale.languageCode);
-        break;
-
-      case LocaleStateStatus.changeApiLocaleFailure:
-        CustomToast.showToast(
-          context: context,
-          messageKey: state.error ?? '',
-          state: CustomToastState.error,
-        );
-        break;
-    }
-  }
 }
