f5a4d4b1b4928fb75bfefdda1166dcf6fadea61e
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-10-29 10:12:28 +0300

    Built ConfirmDeleteAccountBlocConsumer widget & Updated Settings

---
+++ lib/features/profile/presentation/widgets/confirm_delete_account_bloc_consumer.dart
@@ -0,0 +1,43 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:store_ify/core/locale/lang_keys.dart';
+import 'package:store_ify/core/router/app_router.dart';
+import 'package:store_ify/core/utils/functions/circular_indicator_or_text_widget.dart';
+import 'package:store_ify/core/widgets/custom_toast.dart';
+import 'package:store_ify/core/widgets/main_button.dart';
+import 'package:store_ify/features/profile/presentation/cubits/profile_cubit.dart';
+import 'package:store_ify/features/profile/presentation/cubits/profile_state.dart';
+
+class ConfirmDeleteAccountBlocConsumer extends StatelessWidget {
+  const ConfirmDeleteAccountBlocConsumer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<ProfileCubit, ProfileState>(
+      listenWhen: (_, current) =>
+          current is DeleteAccountError || current is DeleteAccountSuccess,
+      listener: (context, state) {
+        state.whenOrNull(
+          deleteAccountError: (error) => CustomToast.showToast(
+            context: context,
+            messageKey: error,
+            state: CustomToastState.error,
+          ),
+          deleteAccountSuccess: () => context.router.popUntil(
+            (route) => route.settings.name == AuthRoute.name,
+          ),
+        );
+      },
+      builder: (context, state) => MainButton(
+        onPressed: () => context.read<ProfileCubit>().deleteAccount(),
+        width: double.infinity,
+        child: circularIndicatorOrTextWidget(
+          isLoading: state is DeleteAccountLoading,
+          context: context,
+          textKey: LangKeys.confirm,
+        ),
+      ),
+    );
+  }
+}
