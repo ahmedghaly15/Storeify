da80c6aff8691e44422514da363073ee9832b2e0
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-11-27 01:12:01 +0200

    Refactored StoreifyApp widget

--- lib/storeify_app.dart
+++ lib/storeify_app.dart
@@ -1,10 +1,14 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:store_ify/core/router/app_router.dart';
 import 'package:store_ify/core/themes/theming_cubit.dart';
 import 'package:store_ify/core/di/dependency_injection.dart';
+import 'package:store_ify/core/utils/app_strings.dart';
+import 'package:store_ify/core/utils/auto_route_observer.dart';
 import 'package:store_ify/features/favorites/presentation/cubits/favorites/favorites_cubit.dart';
-import 'package:store_ify/storeify_material_app.dart';
 
 class StoreifyApp extends StatelessWidget {
   const StoreifyApp({super.key});
@@ -24,7 +28,24 @@ class StoreifyApp extends StatelessWidget {
             create: (_) => getIt.get<ThemingCubit>(),
           ),
         ],
-        child: const StoreifyMaterialApp(),
+        child: BlocBuilder<ThemingCubit, ThemeData>(
+          buildWhen: (previous, current) =>
+              previous.brightness != current.brightness,
+          builder: (context, themeState) => MaterialApp.router(
+            debugShowCheckedModeBanner: false,
+            localizationsDelegates: context.localizationDelegates,
+            supportedLocales: context.supportedLocales,
+            locale: context.locale,
+            theme: themeState,
+            title: AppStrings.appTitle,
+            routerConfig: getIt.get<AppRouter>().config(
+                  navigatorObservers: () => [
+                    AppRoutesObserver(),
+                    AutoRouteObserver(),
+                  ],
+                ),
+          ),
+        ),
       ),
     );
   }
