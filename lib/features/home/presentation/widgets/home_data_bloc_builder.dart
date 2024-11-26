000db7a7df75b1e132cf9f91748aee02d56e535b
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-11-26 23:35:32 +0200

    Refactored home feature

--- lib/features/home/presentation/widgets/home_data_bloc_builder.dart
+++ lib/features/home/presentation/widgets/home_data_bloc_builder.dart
@@ -6,6 +6,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:store_ify/core/router/app_router.dart';
 import 'package:store_ify/core/widgets/custom_error_widget.dart';
 import 'package:store_ify/core/widgets/my_sized_box.dart';
+import 'package:store_ify/features/home/data/models/fetch_home_response.dart';
 import 'package:store_ify/features/home/presentation/cubit/home_cubit.dart';
 import 'package:store_ify/features/home/presentation/cubit/home_state.dart';
 import 'package:store_ify/features/home/presentation/widgets/best_selling_list_view.dart';
@@ -22,46 +23,68 @@ class HomeDataBlocBuilder extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<HomeCubit, HomeState>(
-      buildWhen: (_, current) =>
-          current is FetchHomeDataLoading ||
-          current is FetchHomeDataSuccess ||
-          current is FetchHomeDataError,
-      builder: (context, state) => state.maybeWhen(
-        fetchHomeDataLoading: () => const HomeShimmerLoading(),
-        fetchHomeDataError: (errorKey) => CustomErrorWidget(
-          tryAgainOnPressed: () => context.read<HomeCubit>().fetchHomeData(),
-          errorKey: errorKey,
-        ),
-        fetchHomeDataSuccess: (homeData) => Container(
-          margin: EdgeInsetsDirectional.only(bottom: 16.h),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const ListTitle(
-                titleKey: LocaleKeys.bestSelling,
-                bottomPadding: 25,
-              ),
-              BestSellingListView(bestSelling: homeData.bestSelling),
-              MySizedBox.height13,
-              PaddedTitleAndViewAllTextButton(
-                titleKey: LocaleKeys.categories,
-                viewAllOnPressed: () =>
-                    context.pushRoute(const CategoriesRoute()),
-              ),
-              CategoriesListView(categories: homeData.categories),
-              MySizedBox.height13,
-              PaddedTitleAndViewAllTextButton(
-                titleKey: LocaleKeys.topStores,
-                viewAllOnPressed: () {},
-              ),
-              TopStoresListView(topStores: homeData.topStores),
-            ],
+      buildWhen: (_, current) => _buildWhen(current.status),
+      builder: (context, state) {
+        switch (state.status) {
+          case HomeStateStatus.fetchHomeDataLoading:
+            return const HomeShimmerLoading();
+          case HomeStateStatus.fetchHomeDataError:
+            return state.homeData != null
+                ? HomeDataWidget(homeData: state.homeData!)
+                : CustomErrorWidget(
+                    tryAgainOnPressed: () =>
+                        context.read<HomeCubit>().fetchHomeData(),
+                    errorKey: state.error!,
+                  );
+          case HomeStateStatus.fetchHomeDataSuccess:
+            return HomeDataWidget(homeData: state.homeData!);
+          default:
+            return const HomeShimmerLoading();
+        }
+      },
+    );
+  }
+
+  bool _buildWhen(HomeStateStatus status) {
+    return status == HomeStateStatus.fetchHomeDataLoading ||
+        status == HomeStateStatus.fetchHomeDataSuccess ||
+        status == HomeStateStatus.fetchHomeDataError;
+  }
+}
+
+class HomeDataWidget extends StatelessWidget {
+  const HomeDataWidget({
+    super.key,
+    required this.homeData,
+  });
+
+  final FetchHomeResponse homeData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsetsDirectional.only(bottom: 16.h),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const ListTitle(
+            titleKey: LocaleKeys.bestSelling,
+            bottomPadding: 25,
           ),
-        ),
-        orElse: () => CustomErrorWidget(
-          tryAgainOnPressed: () => context.read<HomeCubit>().fetchHomeData(),
-          errorKey: LocaleKeys.defaultError,
-        ),
+          BestSellingListView(bestSelling: homeData.bestSelling),
+          MySizedBox.height13,
+          PaddedTitleAndViewAllTextButton(
+            titleKey: LocaleKeys.categories,
+            viewAllOnPressed: () => context.pushRoute(const CategoriesRoute()),
+          ),
+          CategoriesListView(categories: homeData.categories),
+          MySizedBox.height13,
+          PaddedTitleAndViewAllTextButton(
+            titleKey: LocaleKeys.topStores,
+            viewAllOnPressed: () {},
+          ),
+          TopStoresListView(topStores: homeData.topStores),
+        ],
       ),
     );
   }
