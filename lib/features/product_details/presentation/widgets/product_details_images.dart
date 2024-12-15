5c7dae73ee743c0858cb846db1116d6b18691de9
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-12-15 17:11:24 +0200

    Updated ProductDetailsImages widget  - Added shadows to images

--- lib/features/product_details/presentation/widgets/product_details_images.dart
+++ lib/features/product_details/presentation/widgets/product_details_images.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:store_ify/core/models/product.dart';
+import 'package:store_ify/core/utils/app_constants.dart';
 import 'package:store_ify/core/widgets/custom_cached_network_image.dart';
 
 class ProductDetailsImages extends StatelessWidget {
@@ -18,10 +19,7 @@ class ProductDetailsImages extends StatelessWidget {
             children: [
               CustomCachedNetworkImage(
                 imageUrl: productImages[0].img,
-                imageBuilder: (_, img) => ClipRRect(
-                  borderRadius: BorderRadius.circular(50.r),
-                  child: Image(image: img),
-                ),
+                imageBuilder: (_, img) => CustomProductImage(img: img),
               ),
               Padding(
                 padding: EdgeInsetsDirectional.only(end: 20.w),
@@ -31,9 +29,16 @@ class ProductDetailsImages extends StatelessWidget {
                     productImages.length - 1,
                     (index) => CustomCachedNetworkImage(
                       imageUrl: productImages[index + 1].img,
-                      imageBuilder: (_, img) => CircleAvatar(
-                        backgroundImage: img,
-                        radius: 25.r,
+                      imageBuilder: (_, img) => Container(
+                        height: 50.h,
+                        width: 50.h,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(image: img, fit: BoxFit.cover),
+                          boxShadow: [
+                            AppConstants.itemBoxShadow,
+                          ],
+                        ),
                       ),
                     ),
                   ),
@@ -44,11 +49,32 @@ class ProductDetailsImages extends StatelessWidget {
         : Center(
             child: CustomCachedNetworkImage(
               imageUrl: productImages[0].img,
-              imageBuilder: (_, img) => ClipRRect(
-                borderRadius: BorderRadius.circular(50.r),
-                child: Image(image: img),
-              ),
+              imageBuilder: (_, img) => CustomProductImage(img: img),
             ),
           );
+  }
+}
+
+class CustomProductImage extends StatelessWidget {
+  const CustomProductImage({
+    super.key,
+    required this.img,
+  });
+
+  final ImageProvider<Object> img;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 205.h,
+      width: 205.h,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(50.r),
+        image: DecorationImage(image: img, fit: BoxFit.cover),
+        boxShadow: [
+          AppConstants.itemBoxShadow,
+        ],
+      ),
+    );
   }
 }
