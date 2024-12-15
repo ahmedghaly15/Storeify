e0fb4eb79ab543589de5921cba591bfd9a747fde
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-12-15 22:31:19 +0200

    Updated CartProductItem, ProductDetailsPrice, ProductDetailsViewBody widgets  - Used increase/decrease ProductQuantity UI logic for each

--- lib/features/product_details/presentation/widgets/product_details_price.dart
+++ lib/features/product_details/presentation/widgets/product_details_price.dart
@@ -3,8 +3,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:store_ify/core/themes/app_colors.dart';
 import 'package:store_ify/core/themes/app_text_styles.dart';
-import 'package:store_ify/features/favorites/presentation/cubits/favorites/general_cubit.dart';
-import 'package:store_ify/features/favorites/presentation/cubits/favorites/general_state.dart';
+import 'package:store_ify/features/product_details/presentation/cubit/product_details_cubit.dart';
+import 'package:store_ify/features/product_details/presentation/cubit/product_details_state.dart';
 
 class ProductDetailsPrice extends StatelessWidget {
   const ProductDetailsPrice({
@@ -21,7 +21,7 @@ class ProductDetailsPrice extends StatelessWidget {
       spacing: 32.w,
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        BlocSelector<GeneralCubit, GeneralState, int>(
+        BlocSelector<ProductDetailsCubit, ProductDetailsState, int>(
           selector: (state) => state.productQuantity,
           builder: (context, productQuantity) => Text(
             '${(priceAfterDiscount * productQuantity).toStringAsFixed(2)} LE',
--- lib/features/product_details/presentation/widgets/product_details_view_body.dart
+++ lib/features/product_details/presentation/widgets/product_details_view_body.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:store_ify/core/models/product.dart';
 import 'package:store_ify/core/themes/app_colors.dart';
 import 'package:store_ify/core/themes/app_text_styles.dart';
 import 'package:store_ify/core/widgets/custom_sliver_app_bar.dart';
 import 'package:store_ify/core/widgets/product_quantity_controller.dart';
+import 'package:store_ify/features/product_details/presentation/cubit/product_details_cubit.dart';
+import 'package:store_ify/features/product_details/presentation/cubit/product_details_state.dart';
 import 'package:store_ify/features/product_details/presentation/widgets/product_description_and_action_buttons.dart';
 import 'package:store_ify/features/product_details/presentation/widgets/product_details_images.dart';
 import 'package:store_ify/features/product_details/presentation/widgets/product_details_price.dart';
@@ -50,9 +53,23 @@ class ProductDetailsViewBody extends StatelessWidget {
               ),
             ),
           ),
-          const SliverToBoxAdapter(
+          SliverToBoxAdapter(
             child: ProductQuantityController(
               mainAxisAlignment: MainAxisAlignment.center,
+              decreaseOnPressed: () =>
+                  context.read<ProductDetailsCubit>().decreaseProductQuantity(),
+              increaseOnPressed: () =>
+                  context.read<ProductDetailsCubit>().increaseProductQuantity(),
+              productQuantityWidget:
+                  BlocSelector<ProductDetailsCubit, ProductDetailsState, int>(
+                selector: (state) => state.productQuantity,
+                builder: (context, productQuantity) => Text(
+                  '$productQuantity',
+                  style: AppTextStyles.textStyle8Regular.copyWith(
+                    color: AppColors.primaryColor,
+                  ),
+                ),
+              ),
             ),
           ),
           if (product.sizes != null && product.sizes!.isNotEmpty)
