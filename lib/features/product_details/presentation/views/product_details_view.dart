31ddd2b79d995de04aaa21cff69bcef10f4c0c71
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-12-15 17:12:33 +0200

    Finished product-details UI  - Finished ProductDetailsView widget

--- lib/features/product_details/presentation/views/product_details_view.dart
+++ lib/features/product_details/presentation/views/product_details_view.dart
@@ -5,9 +5,13 @@ import 'package:store_ify/core/models/product.dart';
 import 'package:store_ify/core/themes/app_colors.dart';
 import 'package:store_ify/core/themes/app_text_styles.dart';
 import 'package:store_ify/core/widgets/custom_sliver_app_bar.dart';
+import 'package:store_ify/core/widgets/main_button.dart';
 import 'package:store_ify/core/widgets/product_quantity_controller.dart';
 import 'package:store_ify/features/product_details/presentation/widgets/product_details_images.dart';
 import 'package:store_ify/features/product_details/presentation/widgets/product_details_price.dart';
+import 'package:store_ify/features/product_details/presentation/widgets/product_details_section_title.dart';
+import 'package:store_ify/features/product_details/presentation/widgets/product_details_sizes_and_prefer_icon_button.dart';
+import 'package:store_ify/generated/locale_keys.g.dart';
 
 @RoutePage()
 class ProductDetailsView extends StatelessWidget {
@@ -49,6 +53,98 @@ class ProductDetailsView extends StatelessWidget {
             const SliverToBoxAdapter(
               child: ProductQuantityController(
                 mainAxisAlignment: MainAxisAlignment.center,
+              ),
+            ),
+            if (product.sizes != null && product.sizes!.isNotEmpty)
+              SliverPadding(
+                padding: EdgeInsets.symmetric(horizontal: 32.w),
+                sliver: const SliverToBoxAdapter(
+                  child: ProductDetailsSectionTitle(
+                    titleKey: LocaleKeys.sizes,
+                  ),
+                ),
+              ),
+            SliverPadding(
+              padding: EdgeInsets.symmetric(horizontal: 32.w),
+              sliver: SliverToBoxAdapter(
+                child: ProductDetailsSizesAndPreferIconButton(product: product),
+              ),
+            ),
+            if (product.colors != null && product.colors!.isNotEmpty)
+              SliverPadding(
+                padding: EdgeInsets.symmetric(horizontal: 32.w),
+                sliver: const SliverToBoxAdapter(
+                  child: ProductDetailsSectionTitle(
+                    titleKey: LocaleKeys.colors,
+                  ),
+                ),
+              ),
+            if (product.colors != null && product.colors!.isNotEmpty)
+              SliverPadding(
+                padding: EdgeInsets.symmetric(horizontal: 32.w),
+                sliver: SliverToBoxAdapter(
+                  child: Row(
+                    spacing: 8.w,
+                    children: List.generate(
+                      product.colors!.length,
+                      (index) => Text(
+                        product.colors![index].color,
+                        style: AppTextStyles.textStyle16Regular,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            SliverPadding(
+              padding: EdgeInsets.symmetric(horizontal: 32.w),
+              sliver: const SliverToBoxAdapter(
+                child: ProductDetailsSectionTitle(
+                  titleKey: LocaleKeys.details,
+                ),
+              ),
+            ),
+            SliverPadding(
+              padding: EdgeInsets.symmetric(horizontal: 32.w),
+              sliver: SliverFillRemaining(
+                hasScrollBody: false,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      product.description,
+                      style: AppTextStyles.textStyle16Regular,
+                    ),
+                    const Spacer(),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 12.h),
+                      child: Row(
+                        spacing: 13.w,
+                        children: [
+                          Expanded(
+                            child: MainButton(
+                              margin: EdgeInsets.zero,
+                              borderRadius: 8,
+                              textKey: LocaleKeys.addToCart,
+                              isOutlined: true,
+                              onPressed: () {
+                                debugPrint(
+                                    product.sizes!.isNotEmpty.toString());
+                              },
+                            ),
+                          ),
+                          Expanded(
+                            child: MainButton(
+                              margin: EdgeInsets.zero,
+                              borderRadius: 8,
+                              textKey: LocaleKeys.buyNow,
+                              onPressed: () {},
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
