bcac4386b9111b0043faf5cc8162ab9c0a481958
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-12-15 22:31:52 +0200

    Provided ProductDetailsCubit for product-details feature

--- lib/features/product_details/presentation/views/product_details_view.dart
+++ lib/features/product_details/presentation/views/product_details_view.dart
@@ -1,13 +1,24 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:store_ify/core/di/dependency_injection.dart';
 import 'package:store_ify/core/models/product.dart';
+import 'package:store_ify/features/product_details/presentation/cubit/product_details_cubit.dart';
 import 'package:store_ify/features/product_details/presentation/widgets/product_details_view_body.dart';
 
 @RoutePage()
-class ProductDetailsView extends StatelessWidget {
+class ProductDetailsView extends StatelessWidget implements AutoRouteWrapper {
   const ProductDetailsView({super.key, required this.product});
 
   final Product product;
+
+  @override
+  Widget wrappedRoute(BuildContext context) {
+    return BlocProvider<ProductDetailsCubit>(
+      create: (_) => getIt.get<ProductDetailsCubit>(),
+      child: this,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
