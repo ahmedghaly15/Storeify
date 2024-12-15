9da1b3fe3c2867bab8683d2d69c6c068b45aa55f
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-12-16 00:56:23 +0200

    Updated ProductDetailsSizesAndPreferIconButton widget  - Handled selecting a product size

--- lib/features/product_details/presentation/widgets/product_details_sizes_and_prefer_icon_button.dart
+++ lib/features/product_details/presentation/widgets/product_details_sizes_and_prefer_icon_button.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:store_ify/core/helpers/extensions.dart';
 import 'package:store_ify/core/models/product.dart';
+import 'package:store_ify/core/models/product_size.dart';
+import 'package:store_ify/core/themes/app_colors.dart';
 import 'package:store_ify/core/themes/app_text_styles.dart';
 import 'package:store_ify/core/widgets/prefer_product_bloc_listener_icon_button.dart';
+import 'package:store_ify/features/product_details/presentation/cubit/product_details_cubit.dart';
+import 'package:store_ify/features/product_details/presentation/cubit/product_details_state.dart';
 
 class ProductDetailsSizesAndPreferIconButton extends StatelessWidget {
   const ProductDetailsSizesAndPreferIconButton({
@@ -19,12 +25,34 @@ class ProductDetailsSizesAndPreferIconButton extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Row(
-                spacing: 6.w,
                 children: List.generate(
                   product.sizes!.length,
-                  (index) => Text(
-                    product.sizes![index].size,
-                    style: AppTextStyles.textStyle16Regular,
+                  (index) => BlocSelector<ProductDetailsCubit,
+                      ProductDetailsState, ProductSize>(
+                    selector: (state) => state.selectedProductSize!,
+                    builder: (context, selectedProductSize) {
+                      final isSelected =
+                          selectedProductSize == product.sizes![index];
+                      return TextButton(
+                        style: TextButton.styleFrom(
+                          minimumSize: Size.zero,
+                          padding: EdgeInsets.all(8.h),
+                          tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                          foregroundColor: isSelected
+                              ? AppColors.primaryColor
+                              : (context.isDarkModeActive
+                                  ? Colors.white
+                                  : Colors.black),
+                          textStyle: AppTextStyles.textStyle16Regular,
+                        ),
+                        onPressed: () => context
+                            .read<ProductDetailsCubit>()
+                            .selectProductSize(
+                              product.sizes![index],
+                            ),
+                        child: Text(product.sizes![index].size),
+                      );
+                    },
                   ),
                 ),
               ),
