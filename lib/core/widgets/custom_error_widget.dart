69b75fb7439d77f6648634313e87fe0783c4a493
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-12-16 21:52:01 +0200

    Updated CustomErrorWidget  - Deleted not neccessary try_again_button.dart file

--- lib/core/widgets/custom_error_widget.dart
+++ lib/core/widgets/custom_error_widget.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:store_ify/core/themes/app_text_styles.dart';
 import 'package:store_ify/core/utils/app_assets.dart';
-import 'package:store_ify/core/widgets/try_again_button.dart';
+import 'package:store_ify/core/widgets/main_button.dart';
 import 'package:store_ify/generated/locale_keys.g.dart';
 
 class CustomErrorWidget extends StatelessWidget {
@@ -53,6 +53,23 @@ class CustomErrorWidget extends StatelessWidget {
           if (tryAgainOnPressed != null)
             TryAgainButton(onPressed: tryAgainOnPressed),
         ],
+      ),
+    );
+  }
+}
+
+class TryAgainButton extends StatelessWidget {
+  const TryAgainButton({super.key, required this.onPressed});
+
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: MainButton(
+        margin: EdgeInsets.symmetric(horizontal: 80.w),
+        onPressed: onPressed,
+        textKey: LocaleKeys.tryAgain,
       ),
     );
   }
