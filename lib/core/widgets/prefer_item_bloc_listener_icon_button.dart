0157db3aad49255d284db1831c75dec7957c6130
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-12-16 21:48:34 +0200

    Built PreferItemBlocListenerIconButton widget  - Deleted PreferProductBlocListenerIconButton, PreferStoreBlocListenerIconButton widgets

--- lib/core/widgets/prefer_item_bloc_listener_icon_button.dart
+++ lib/core/widgets/prefer_item_bloc_listener_icon_button.dart
@@ -1,28 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:store_ify/core/helpers/enums.dart';
 import 'package:store_ify/core/helpers/extensions.dart';
 import 'package:store_ify/core/themes/app_colors.dart';
 import 'package:store_ify/features/favorites/presentation/cubits/favorites/general_cubit.dart';
 import 'package:store_ify/features/favorites/presentation/cubits/favorites/general_state.dart';
 
-class PreferProductBlocListenerIconButton extends StatefulWidget {
-  const PreferProductBlocListenerIconButton({
+class PreferItemBlocListenerIconButton extends StatefulWidget {
+  const PreferItemBlocListenerIconButton({
     super.key,
     required this.isFavorited,
-    required this.productId,
+    this.productId,
+    this.storeId,
+    required this.itemType,
   });
 
   final bool isFavorited;
-  final int productId;
+  final int? productId, storeId;
+  final FavItemType itemType;
 
   @override
-  State<PreferProductBlocListenerIconButton> createState() =>
-      _PreferProductBlocListenerIconButtonState();
+  State<PreferItemBlocListenerIconButton> createState() =>
+      _PreferItemBlocListenerIconButtonState();
 }
 
-class _PreferProductBlocListenerIconButtonState
-    extends State<PreferProductBlocListenerIconButton> {
+class _PreferItemBlocListenerIconButtonState
+    extends State<PreferItemBlocListenerIconButton> {
   late bool _isFavoritedLocal;
 
   @override
@@ -31,12 +35,17 @@ class _PreferProductBlocListenerIconButtonState
     _isFavoritedLocal = widget.isFavorited;
   }
 
-  void _preferProductOrNot() {
+  void _preferItemOrNot() {
     _toggleIsFavoritedLocal();
-    context.read<GeneralCubit>().preferProductOrNot(
-          productId: widget.productId,
-          isFavorited: _isFavoritedLocal,
-        );
+    widget.itemType == FavItemType.product
+        ? context.read<GeneralCubit>().preferProductOrNot(
+              productId: widget.productId!,
+              isFavorited: _isFavoritedLocal,
+            )
+        : context.read<GeneralCubit>().preferStoreOrNot(
+              storeId: widget.storeId!,
+              isFavorited: _isFavoritedLocal,
+            );
   }
 
   void _toggleIsFavoritedLocal() {
@@ -51,7 +60,7 @@ class _PreferProductBlocListenerIconButtonState
       listenWhen: (_, current) => _listenWhen(current.status),
       listener: (context, state) => _listener(state, context),
       child: IconButton(
-        onPressed: () => _preferProductOrNot(),
+        onPressed: () => _preferItemOrNot(),
         icon: Icon(
           _isFavoritedLocal ? Icons.favorite : Icons.favorite_border_outlined,
           size: 18.w,
@@ -63,12 +72,10 @@ class _PreferProductBlocListenerIconButtonState
 
   void _listener(GeneralState state, BuildContext context) {
     switch (state.status) {
-      case GeneralStateStatus.removeProductFromFavsError:
-        context.showToast(state.error!);
-        // Rollback the change if an error occurs
-        _toggleIsFavoritedLocal();
-        break;
       case GeneralStateStatus.preferProductError:
+      case GeneralStateStatus.removeProductFromFavsError:
+      case GeneralStateStatus.preferStoreError:
+      case GeneralStateStatus.removeStoreFromFavsError:
         context.showToast(state.error!);
         // Rollback the change if an error occurs
         _toggleIsFavoritedLocal();
@@ -76,6 +83,10 @@ class _PreferProductBlocListenerIconButtonState
       case GeneralStateStatus.preferProductSuccess:
       case GeneralStateStatus.removeProductFromFavsSuccess:
         context.read<GeneralCubit>().deleteCachedFavProducts();
+        break;
+      case GeneralStateStatus.preferStoreSuccess:
+      case GeneralStateStatus.removeStoreFromFavsSuccess:
+        context.read<GeneralCubit>().deleteCachedFavStores();
         break;
       default:
         break;
@@ -86,6 +97,10 @@ class _PreferProductBlocListenerIconButtonState
     return status == GeneralStateStatus.removeProductFromFavsError ||
         status == GeneralStateStatus.preferProductError ||
         status == GeneralStateStatus.preferProductSuccess ||
-        status == GeneralStateStatus.removeProductFromFavsSuccess;
+        status == GeneralStateStatus.removeProductFromFavsSuccess ||
+        status == GeneralStateStatus.preferStoreError ||
+        status == GeneralStateStatus.removeStoreFromFavsError ||
+        status == GeneralStateStatus.preferStoreSuccess ||
+        status == GeneralStateStatus.removeStoreFromFavsSuccess;
   }
 }
